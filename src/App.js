f3426bef84f956faf7d3ec48dc73983e98285f44
Mohak Jain <jmohak30@gmail.com>
2025-01-14 00:06:02 +0530

    Create Admin Dashboard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Login from './components/Auth/Login'
+import AdminDashboard from './components/Dashboard/AdminDashboard';
 import EmployessDashboard from './components/Dashboard/EmployessDashboard';
 
 function App() {
   return (
     <>
      {/* <Login/> */}
-     <EmployessDashboard/>
+     {/* <EmployessDashboard/> */}
+     <AdminDashboard/>
     </>
   );
 }
---
+++ src/components/Others/AllTask.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function AllTask() {
+    return (
+        <div className='bg-[#1c1c1c] p-5 rounded mt-5 h-50 overflow-auto'>
+            <div className='flex justify-between mb-2 rounded bg-red-400 py-2 px-4'>
+                <h2>Sarthak</h2>
+                <h3>Make a UI Design</h3>
+                <h5>Status</h5>
+            </div>
+            <div className='flex justify-between mb-2 rounded bg-green-400 py-2 px-4'>
+                <h2>Sarthak</h2>
+                <h3>Make a UI Design</h3>
+                <h5>Status</h5>
+            </div>
+            <div className='flex justify-between mb-2 rounded bg-blue-400 py-2 px-4'>
+                <h2>Sarthak</h2>
+                <h3>Make a UI Design</h3>
+                <h5>Status</h5>
+            </div>
+            <div className='flex justify-between mb-2 rounded bg-yellow-400 py-2 px-4'>
+                <h2>Sarthak</h2>
+                <h3>Make a UI Design</h3>
+                <h5>Status</h5>
+            </div>
+            <div className='flex justify-between mb-2 rounded bg-purple-400 py-2 px-4'>
+                <h2>Sarthak</h2>
+                <h3>Make a UI Design</h3>
+                <h5>Status</h5>
+            </div>
+            
+        </div>
+    )
+}
+
+export default AllTask
